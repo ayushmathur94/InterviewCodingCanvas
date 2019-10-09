f8eed04fc1732bc2068b41fd916729efc383f51e
Ayush Mathur <ayushmathur1994@gmail.com>
2019-10-10 04:38:51 +0530

    Code for minlen of req subarray committed P11
    P11 sol.

--- src/InterviewCode/SubArrayMinLength_P11.java
+++ src/InterviewCode/SubArrayMinLength_P11.java
@@ -1,5 +1,7 @@
 package InterviewCode;
 
+import java.util.Arrays;
+
 //11
 /*Given an array of n positive integers and a positive integer s , find the minimal length of a subarray of which the
 *sum >= s . If there isn't one , return 0 instead.
@@ -8,6 +10,49 @@ package InterviewCode;
 
 public class SubArrayMinLength_P11 {
 
+	public static int minSubArrayLen(int s, int[] arr){
+		if(arr == null || arr.length == 0){
+			return 0 ;
+		}
+		
+		int i = 0 ;
+		int j = 0;
+		int sum = 0;
+		
+		int minLen = Integer.MAX_VALUE; 
+		
+		while(j < arr.length){
+			if(sum<s){
+				sum += arr[j];
+				j++ ;
+			}else{
+				minLen = Math.min(minLen, j-i);
+
+				if(i == j-1){
+					return 1;
+				}
+				sum -= arr[i];	
+				i++;
+			}
+		}
+			
+			while(sum >= s){
+				minLen  = 	Math.min(minLen, j-i);
+				sum -=arr[i++];
+			}
+			return minLen == Integer.MAX_VALUE ?  0 : minLen;
+
+	}
+	
+ public static void main(String[] args) {
+		int array[] = {2,3,1,2,4,3};
+		
+		   int num =7;
+		   
+		   Arrays.sort(array);
+		   System.out.println(minSubArrayLen(num, array));
+	
+}
 	
 	
 }
