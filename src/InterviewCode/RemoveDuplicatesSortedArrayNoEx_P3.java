89d7444618f49705a2549d0cce81ade45a95c65f
Ayush Mathur <ayushmathur1994@gmail.com>
2021-08-12 00:35:52 +0530

    Update RemoveDuplicatesSortedArrayNoEx_P3.java

--- src/InterviewCode/RemoveDuplicatesSortedArrayNoEx_P3.java
+++ src/InterviewCode/RemoveDuplicatesSortedArrayNoEx_P3.java
@@ -12,7 +12,7 @@ public class RemoveDuplicatesSortedArrayNoEx_P3 {
 	  
 	  int j = 0 ;
 	  
-	  for(int i = 0 ; i <n-1 ; i ++ ){
+	  for(int i = 0 ; i <n-1 ; i++ ){
 		  if(arr[i]!=arr[i+1]){
 			  arr[j] = arr[i];
 			  j++ ;
